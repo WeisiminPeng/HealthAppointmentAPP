74b59c61d91ecd398312cb063733ca9525c4c56e
WeisiminPeng <59750029+WeisiminPeng@users.noreply.github.com>
2020-04-11 23:39:29 -0400

    add patient sidebar

--- HealthAppointmentAPP/src/app/HealthAppointment/home/patient/patient-sidebar/patient-sidebar.component.ts
+++ HealthAppointmentAPP/src/app/HealthAppointment/home/patient/patient-sidebar/patient-sidebar.component.ts
@@ -13,9 +13,7 @@ export class PatientSidebarComponent implements OnInit {
   }
 
   onItemClick(args: any) {
-    // if (Browser.isDevice) {
-    //   this.sideBar.hide();
-    // }
+  
     const elements: HTMLElement[] = args.currentTarget.parentElement.querySelectorAll('.active-item');
     elements.forEach(element => {
       if (element.classList.contains('active-item')) { element.classList.remove('active-item'); }
